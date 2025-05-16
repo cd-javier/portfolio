ebc371606cf6e06cb39127323e1597980bf673b4
Javier Quiroga <contact@javierquiroga.com>
2025-05-16 20:02:33 +0100

    feat: dev navbar

---
+++ script/menu.js
@@ -0,0 +1,27 @@
+const navToggle = document.getElementById('nav-toggle');
+const dropdown = document.getElementById('dropdown');
+
+function toggleDropdown() {
+  if (dropdown.style.display === 'flex') {
+    dropdown.style.display = 'none';
+  } else {
+    dropdown.style.display = 'flex';
+  }
+}
+
+function closeDropdown(e) {
+  const target = e.target.closest('a');
+  if (target) {
+    dropdown.style.display = 'none';
+  }
+}
+
+navToggle.addEventListener('click', toggleDropdown);
+dropdown.addEventListener('click', closeDropdown);
+window.addEventListener('resize', () => {
+  if (window.innerWidth >= 768) {
+    dropdown.style.display = 'flex'; // Ensure nav is visible when switching back to desktop
+  } else {
+    dropdown.style.display = 'none'; // Keep mobile behavior intact
+  }
+});
